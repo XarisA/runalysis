b651f9d54de673e53dd83d8500b3083b803d514f
Xaris <xaris@gmx.com>
2017-02-20 16:42:27 +0200

    Added some basic Plots
    HR vs Distance, HR Zones pie

--- runalysis.py
+++ runalysis.py
@@ -1,76 +1,68 @@
 import os
-#import time
-#import numpy
-#import datetime
-#import dateutil.parser as dp
-import tcxparser as tp
 import matplotlib.pyplot as plt
+from tcx2dict import *
 
-
-FILEDIRECTORY='./data'
+FILEDIRECTORY='./example_data'
 FILENAME='act1.tcx'
 FILEPATH=os.path.join(FILEDIRECTORY, FILENAME)
 
-dictd={}
-TCX=tp.TCXParser(FILEPATH)
+#Write a function for the heart rate zone grouping
+#make it a class
+#add some methods
 
-#Change getdata function to Recursion, to lower complexity.---Done
-#Remove Global variables.
-#Pass TCX as input in getdata
-#Start ploting
-
-def old_getdata():
-    "Return a dictionary contains all the data from an xml file, having as key a tupple with Lap#,Trackpoint# combination"
-    dictd={}
-    global TCX
-    for i in range(0,len(TCX.activity.Lap)):
-        for j in range (0,len(TCX.activity.Lap[i].Track.Trackpoint)):
-            dictd[i,j]=[TCX.activity.Lap[i].Track.Trackpoint[j].Time,TCX.activity.Lap[i].Track.Trackpoint[j].DistanceMeters,\
-            TCX.activity.Lap[i].Track.Trackpoint[j].HeartRateBpm.Value]
-        return dictd
-
-
-def getdata(n=0,m=0):
-    """
-    Store tcx data in a dictionary having for key a Lap#,measurement# tupple.
-    """
-    try:
-        global TCX
-        global dictd
-        dictd[n,m]=[TCX.activity.Lap[n].Track.Trackpoint[m].Time,TCX.activity.Lap[n].Track.Trackpoint[m].DistanceMeters,\
-                    TCX.activity.Lap[n].Track.Trackpoint[m].HeartRateBpm.Value]
-        if n==len(TCX.activity.Lap)-1:
-            if m == len(TCX.activity.Lap[n].Track.Trackpoint)-1:
-                return dictd
-            else:
-                return getdata(n,m+1)
-        elif n<len(TCX.activity.Lap)-1:
-            if m == len(TCX.activity.Lap[n].Track.Trackpoint)-1:
-                return getdata(n+1,0)
-            else:
-                return getdata(n,m+1)
-        else:
-            raise Exception('General exceptions..')
-    except ValueError as e:
-        print('Value Error!?')
-
-#Test my data in dict
-#test_case=old_getdata(FILEPATH)
-test_case=getdata()
-print test_case
-
-def plot_hr_dis(TCX):
-    #Create time list from dict for the x axis
-
-    for i in range(0, len(TCX.hr_values())):
-        plt.bar(i,TCX.hr_values()[i],1.1, color="brown")
-        #plt.plot(i,TCX.hr_values()[i],'b.')
-    plt.axis([0, 550, 100, 180])
-    plt.title('Heart Rate Values in relevant time')
+def plot_test(Dict):
+    "Plot HR vs Distance in Bars And simple diagram"
+    x=[]
+    y=[]
+    for key in sorted(Dict):
+        x.append(int(Dict[key][1]))
+        y.append(int(Dict[key][2]))
+    plt.subplot(2,1, 1)
+    plt.bar(x,y,10,color="brown")
+    plt.subplot(2,1, 2)
+    plt.plot(x,y,'r-')
+    #plt.axis([0, 6000, 0, 200])
+    plt.title('Heart Rate Values vs Distance')
     plt.ylabel('Heart Rate Values (bpm)')
-    plt.xlabel('Time xsec')
+    plt.xlabel('Distance (m)')
     plt.show()
 
-#Test my plot
-#plot_hr_dis(TCX)
+def HRperZone(Dict):
+    # Pie chart of HR Zones
+    labels = 'Zone 1', 'Zone 2', 'Zone 3', 'Zone 4','Zone 5','Danger'
+    zone1=0
+    zone2=0
+    zone3=0
+    zone4=0
+    zone5=0
+    zone6=0
+
+    def get_autopct(values):
+        def cs_autopct(apct):
+            suma = sum(values)
+            perc = int(round(apct*suma/100.0))
+            return '{p:.2f}%  ({v:d})'.format(p=apct,v=perc)
+        return cs_autopct
+
+    for key in Dict:
+        if int(Dict[key][2])   < 135: zone1+=1
+        elif int(Dict[key][2]) < 147: zone2+=1
+        elif int(Dict[key][2]) < 159: zone3+=1
+        elif int(Dict[key][2]) < 172: zone4+=1
+        elif int(Dict[key][2]) < 183: zone5+=1
+        else: zone6+=1
+
+    sizes = [zone1, zone2, zone3, zone4,zone5,zone6]
+    explode = (0, 0.3, 0, 0,0,0.6)
+    fig1, ax1 = plt.subplots()
+    ax1.pie(sizes, explode=explode, labels=labels, autopct=get_autopct(sizes),shadow=True, startangle=90)
+    ax1.axis('equal')
+    plt.show()
+
+
+#E=ttd(FILEPATH).dict_data
+#plot_test(E)
+#HRperZone(E)
+
+
 
